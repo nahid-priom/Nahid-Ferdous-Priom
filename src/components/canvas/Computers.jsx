7684c5f11597db9fc69edbdf7c2825de077b13ba
nahid-priom <nahid.priom.06@gmail.com>
2023-11-18 22:37:15 +0600

    ps

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -34,7 +34,7 @@ const ComputersCanvas = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    const mediaQuery = window.matchMedia("(max-width: 1500px)");
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
@@ -60,7 +60,6 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-      className="hidden sm:block"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
