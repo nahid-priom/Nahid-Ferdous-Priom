96e0a6c375cddefe0ab39819835ad631aa6226b2
nahid-priom <nahid.priom.06@gmail.com>
2023-11-20 15:51:35 +0600

    FInal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-96 sm:h-screen mx-auto`}>
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-24 sm:top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-center sm:items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -18,7 +18,7 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915EFF]">PrioM</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} my-2 text-white-100`}>
             Empowering Growth Through Web Development <br />
             User interfaces and web applications
           </p>
