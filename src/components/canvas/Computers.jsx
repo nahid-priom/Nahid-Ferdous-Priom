a0633a14a28ea2ff14978fb3755416ab099db634
nahid-priom <nahid.priom.06@gmail.com>
2023-12-07 00:30:25 +0600

    kama

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,22 +9,26 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      {/* Use a simpler ambient light */}
-      <ambientLight intensity={0.2} />
+  {/* Use a simpler ambient light */}
+  <ambientLight intensity={0.1} />
 
-      {/* Use a simple point light for basic illumination */}
-      <pointLight position={[5, 5, 5]} intensity={0.5} />
+  {/* Use a basic point light for basic illumination */}
+  <pointLight position={[5, 5, 5]} intensity={0.5} />
 
-      {/* Simplified spot light for basic shading */}
-      <spotLight position={[-5, 5, 5]} angle={0.3} penumbra={1} intensity={0.8} />
+  {/* Simplify the spot light for basic shading */}
+  <spotLight position={[-5, 5, 5]} angle={0.3} penumbra={1} intensity={0.5} />
 
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.5 : 0.6}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+  {/* Use a basic material with no shadows */}
+  <meshBasicMaterial attach="material" />
+
+  <primitive
+    object={computer.scene}
+    scale={isMobile ? 0.5 : 0.6}
+    position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+    rotation={[-0.01, -0.2, -0.1]}
+  />
+</mesh>
+
   );
 };
 
@@ -32,29 +36,44 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(true);
 
   useEffect(() => {
-    // Check for mobile devices based on user agent
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
-    setIsMobile(isMobileDevice);
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 1500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
   }, []);
 
   return (
     <Canvas
       frameloop='demand'
-      dpr={window.devicePixelRatio}
+      shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true, alpha: false, antialias: false }}
-     
+      gl={{ preserveDrawingBuffer: true }}
+      className=""
     >
       <Suspense fallback={<CanvasLoader />}>
-        {/* Use a simple orbit controls */}
-        <OrbitControls enableZoom={false} />
-
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
         <Computers isMobile={isMobile} />
       </Suspense>
 
-      {/* Preload assets */}
       <Preload all />
     </Canvas>
   );
